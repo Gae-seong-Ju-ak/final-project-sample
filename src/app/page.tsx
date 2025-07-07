2539cd13a0c52c6b8ebb0337fed5d86d91cd1d0d
toosign <kevinsoo1014@gmail.com>
2025-07-07 10:20:16 +0900

    github action deploy test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1>Final-Project-Sample, build and deploy test<br/>
+    <h1>Final-Project-Sample, build and deploy test!!<br/>
     {new Date().toLocaleDateString()}</h1>
   );
 }
